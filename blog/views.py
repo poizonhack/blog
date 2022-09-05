88c19710ac73862cf126d3aefacfc570250d8631
Poizonhack <kaasharrys@gmail.com>
2022-09-05 21:05:33 +0100

    Create Article view

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-
+from .models import Article
 # Create your views here.
 
 def accueil(request):
-    return render(request,"index.html")
+    list_articles=Article.objects.all()
+    context={"liste_articles":list_articles}
+    return render(request,"index.html",context)
+
+
