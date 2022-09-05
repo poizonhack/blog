c7e09760f8dd1f66dd7f394be0217bab48ee6317
Poizonhack <kaasharrys@gmail.com>
2022-09-05 20:05:53 +0100

    Create Article

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Article(models.Model):
+    title = models.CharField(max_length=50)
+    desc = models.TextField()
+    image = models.ImageField()
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
+
+
+
+def __str__(self):
+    return self.title
